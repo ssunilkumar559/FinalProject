8c7de49e653bff8345668e3987acf2b2980f5699
Hasibul Shishir <dew@hasibuls-mbp.fios-router.home>
2017-04-08 01:38:11 -0400

    Initial Commit

---
+++ ProjectDraft/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ProjectDraft
+//
+//  Created by Hasibul Shishir on 4/8/17.
+//  Copyright © 2017 Hasibul Shishir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ ProjectDraft/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectDraft
+//
+//  Created by Hasibul Shishir on 4/8/17.
+//  Copyright © 2017 Hasibul Shishir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
